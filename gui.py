c3833f5ddcb9856c7d107bdf532937e318f4babe
Teddy <teddybagley18@gmail.com>
2025-02-18 08:57:09 -0700

    initial commit

---
+++ gui.py
@@ -0,0 +1,15 @@
+import tkinter as tk
+
+
+class App(tk.Tk):
+    def __init__(self):
+        super().__init__()
+        self.title('Contrast Suggestor')
+        self.configure(background="black")
+        self.geometry("700x900")
+        self.length = 700
+        self.height = 900
+        self.entry_frame = tk.Frame(self, bg ="white", width=self.length, height = self.height/10)
+        self.display1 = tk.Frame(self, bg = "white", width=self.length - 10, height=(self.height/10)*4)
+        self.display1.grid(row=0, column=0, pady = 5)
+        self.entry_frame.grid(row = 3, column = 0, sticky = tk.S)
